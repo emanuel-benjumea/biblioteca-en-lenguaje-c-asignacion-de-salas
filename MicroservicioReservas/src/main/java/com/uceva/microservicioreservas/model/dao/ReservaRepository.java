c2bb3368082ce304327a1277d57845950f7cfbd6
TruSanGamer <trujillosantiago344@gmail.com>
2023-10-10 23:11:41 -0500

    Reservarepository
    Se crea el dao

---
+++ MicroservicioReservas/src/main/java/com/uceva/microservicioreservas/model/dao/ReservaRepository.java
@@ -0,0 +1,9 @@
+package com.uceva.microservicioreservas.model.dao;
+
+import com.uceva.microservicioreservas.model.entities.Reserva;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReservaRepository extends CrudRepository<Reserva, Long> {
+
+    // Puedes añadir consultas personalizadas si es necesario
+}
