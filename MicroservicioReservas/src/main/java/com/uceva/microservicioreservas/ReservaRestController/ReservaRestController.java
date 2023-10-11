8eb7069f85fa59e8edf64d0296bbb37872d91fa1
TruSanGamer <trujillosantiago344@gmail.com>
2023-10-10 23:11:17 -0500

    Restcontroller
    se agrega el eliminar en el restcontroller

--- MicroservicioReservas/src/main/java/com/uceva/microservicioreservas/ReservaRestController/ReservaRestController.java
+++ MicroservicioReservas/src/main/java/com/uceva/microservicioreservas/ReservaRestController/ReservaRestController.java
@@ -1,14 +1,23 @@
 package com.uceva.microservicioreservas.ReservaRestController;
 
+import com.uceva.microservicioreservas.model.dao.ReservaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReservaRestController {
-    private final ReservaRepository reservaRepository;
+
+   private final ReservaRepository reservaRepository;
 
     @Autowired
     public ReservaRestController(ReservaRepository reservaRepository) {
         this.reservaRepository = reservaRepository;
     }
+@DeleteMapping("/reservas/{id}")
+public void eliminarReserva(@PathVariable Long id) {
+    reservaRepository.deleteById(id);    //crear reserva_repository
+}
 }
 
