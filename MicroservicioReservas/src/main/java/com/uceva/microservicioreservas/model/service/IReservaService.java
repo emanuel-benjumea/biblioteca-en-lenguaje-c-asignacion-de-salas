e0d68e2209cc129771d5a27488c1934fc4523145
TruSanGamer <trujillosantiago344@gmail.com>
2023-10-13 11:11:46 -0500

    IReservaService
    Se crear el ReservaService

--- MicroservicioReservas/src/main/java/com/uceva/microservicioreservas/model/service/IReservaService.java
+++ MicroservicioReservas/src/main/java/com/uceva/microservicioreservas/model/service/IReservaService.java
@@ -1,8 +1,12 @@
 package com.uceva.microservicioreservas.model.service;
 
+import com.uceva.microservicioreservas.model.entities.Reserva;
+
 import java.util.List;
-/*public interface IReservaService extends CrudRepository {
 
-
-    
-}*/
+public interface IReservaService {
+    List<Reserva> findAll();
+    Reserva findById(Long id);
+    Reserva save(Reserva reserva);
+    void delete(Reserva reserva);
+}
